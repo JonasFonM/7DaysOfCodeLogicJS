aafd716af1eb1326d1814aebdb01adaa661fca8c
Jonas <57230582+JonasFonM@users.noreply.github.com>
2023-09-26 11:36:00 -0300

    Adding explicit Typecasting to D1.js

--- D1.js
+++ D1.js
@@ -5,19 +5,19 @@ let sTri="30";
 let nDez=10;
 let sDez="10";
 
-if (nUm == sUm) {
+if (nUm == parseInt(sUm)) {
     console.log("As variáveis nUm e sUm tem o mesmo valor, apesar dos tipos diferentes")
     } else {
     console.log("As variáveis nUm e sUm tem valores diferentes")
     }
 
-if (nTri == sTri) {
+if (nTri == parseInt(sTri)) {
     console.log("As variáveis nTri e sTri tem o mesmo valor, apesar dos tipos diferentes")
     } else {
     console.log("As variáveis nTri e sTri tem valores diferentes")
     }   
 
-if (nDez == sDez) {
+if (nDez == parseInt(sDez)) {
     console.log("As variáveis nDez e sDez tem o mesmo valor, apesar dos tipos diferentes")
     } else {
     console.log("As variáveis nDez e sDez tem valores diferentes")
