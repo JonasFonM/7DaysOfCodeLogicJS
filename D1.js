711eea9ca24d7328903b50377a3a89d36218e672
Jonas <57230582+JonasFonM@users.noreply.github.com>
2023-09-26 11:26:14 -0300

    Adding the inicial logic exercise to D1.js

--- D1.js
+++ D1.js
@@ -1,1 +1,24 @@
-console.log(false == '0');
+let nUm=1;
+let sUm="1";
+let nTri=30;
+let sTri="30";
+let nDez=10;
+let sDez="10";
+
+if (nUm == sUm) {
+    console.log("As variáveis nUm e sUm tem o mesmo valor, apesar dos tipos diferentes")
+    } else {
+    console.log("As variáveis nUm e sUm tem valores diferentes")
+    }
+
+if (nTri == sTri) {
+    console.log("As variáveis nTri e sTri tem o mesmo valor, apesar dos tipos diferentes")
+    } else {
+    console.log("As variáveis nTri e sTri tem valores diferentes")
+    }   
+
+if (nDez == sDez) {
+    console.log("As variáveis nDez e sDez tem o mesmo valor, apesar dos tipos diferentes")
+    } else {
+    console.log("As variáveis nDez e sDez tem valores diferentes")
+    }       
