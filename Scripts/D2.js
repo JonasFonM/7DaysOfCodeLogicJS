8d160f03d76a84704fa00c4b1d0b0160c42c76de
Jonas <57230582+JonasFonM@users.noreply.github.com>
2023-09-28 10:35:37 -0300

    Overall refactoring the uncommited work from last version

--- Scripts/D2.js
+++ Scripts/D2.js
@@ -10,4 +10,12 @@ prompt("Qual linguagem de programação você está estudando?")
 const gosta =
 prompt("Você gosta de estudar "+lingprog+"?");
 
-document.getElementById("greetings").innerHTML = "Olá " + nome + ", você tem " + idade + " anos e já está aprendendo " + lingprog + "!";
+const greetings = "Olá " + nome + ", você tem " + idade + " anos e já está aprendendo " + lingprog + "!";
+
+var congrats = ""
+
+if(gosta.toUpperCase === "SIM"){
+    congrats = "Muito bom! Continue estudado e você terá muito sucesso."
+    } else{
+        congrats = "Ahh que pena... Já tentou aprender outras linguagens?"
+    }
