90d9b978b3860b3d700653e9da35928002fbb3db
Jonas <prothetbrasil@gmail.com>
2023-10-01 06:33:58 -0300

    Rewriting and fixing code logic on D4.js

--- Scripts/D4.js
+++ Scripts/D4.js
@@ -1,11 +1,4 @@
-let num = Math.floor(Math.random() * 11);
-let i = 3;
-let numGuess = [];
-let msg = '';
-
-console.log(num);
-
-changeT = (t) => document.getElementById('t1').innerHTML = t;
+changeT = (t) => document.getElementById('t1').innerText = t;
 
 function toggleBlock(itemId,displayMode) {
     const item = document.getElementById(itemId);
@@ -14,25 +7,30 @@ function toggleBlock(itemId,displayMode) {
   }
 }
 
-function guessNum() {
-    numGuess[i] = prompt("Insira um número de 0 a 10:");
-    i--;
-    msg = 'Você tem '+(i)+' tentativas para adivinhar um número de 0 a 10!';
-    changeT(msg);
-}
+function buttonGuess(){
+    let num = Math.floor(Math.random() * 11);
+    let numGuess = [];
+    let msg = '';
+    let i = 3;
 
-function buttonGuess() {
-    guessNum();
-
-    if (numGuess[i] === num) {
-        toggleBlock('ans1','block');
-        toggleBlock('menu','none');
-        i = 0;            
-    } else if (numGuess =! num && i > 0){
-        changeT(msg)
-    } else if (numGuess =! num && i<=0){
-        toggleBlock('ans2','block');
-        toggleBlock('menu','none');
-    }
-       
+    console.log(num);
+    while (numGuess[i] != num && i > 0) {
+        numGuess[i] = prompt("Insira um número de 0 a 10:");
+        if (numGuess[i] == num) {
+            toggleBlock('menu','none');
+            msg = 'Parabéns, você adivinhou o número corretamente!';
+            changeT(msg);
+            i = -1;            
+        } else if (numGuess != num && i > 0){
+            i--;
+            alert('Você tem '+(i)+' tentativas para adivinhar o número!');
+            if(numGuess != num && i == 0) {
+                toggleBlock('menu','none');
+                msg = 'Que pena, não foi dessa vez, abra a página mais tarde e tente de novo!';
+                changeT(msg);
+                i = -1;
+            }
+        } 
+    } 
+                  
 }
